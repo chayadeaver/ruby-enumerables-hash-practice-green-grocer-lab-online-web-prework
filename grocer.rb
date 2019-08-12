5314b91f848d16370150036a372f17a7024e1afd
Chaya Deaver <chayadeaver@gmail.com>
2019-08-12 02:34:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,23 @@ cart
 end
 
 def checkout(cart, coupons)
-   
+    #call the consolidate to get the count item first
+  new_cart = consolidate_cart(cart)
+  #apply coupon to the new cart
+  apply_coupons(new_cart, coupons)
+  #apply clearance after discount from coupon
+  apply_clearance(new_cart)
+
+total = 0
+  new_cart.each do |name, hash|
+    total += (hash[:price] * hash[:count])
+  end
+
+if total >= 100
+    total *= 0.9
+  end
+
+  total 
 
 end
 

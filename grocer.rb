46f017480a51f61e3ad366c9d7555c6905d262ba
Chaya Deaver <chayadeaver@gmail.com>
2019-08-12 01:02:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  cart.each do |item_array| 
+    # binding.pry
+    item_array.each do |item, attribute_hash|
+      consolidated_cart[item] ||= attribute_hash
+      consolidated_cart[item][:count] ? consolidated_cart[item][:count] +=1 : consolidated_cart[item][:count] = 1
+    end
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)

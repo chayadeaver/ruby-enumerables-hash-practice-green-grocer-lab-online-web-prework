5a185e57c4037998cc09b7c71528118cb48ee768
Chaya Deaver <chayadeaver@gmail.com>
2019-08-12 01:32:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,7 +13,19 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  
+  coupons.each do |coupons|
+    coupons.each do |attribute, value|
+      name = coupon[:items]
+      
+      if cart[name] && car[name][:count] >= coupon[:num]
+        if cart["#{name} W/COUPON"] cart["#{name} W/COUPON"][:count] +=1 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count =>1}
+        end
+      cart[name][:count] -= coupon[:num]
+      end
+    end
+  end
+cart
 end
 
 def apply_clearance(cart)

38e7e56965b34464fe04d598482d875cfe127dce
Chaya Deaver <chayadeaver@gmail.com>
2019-08-12 02:32:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,23 +13,30 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coupons.each do |coupon| 
-    coupon.each do |attribute, value| 
-      name = coupon[:item] 
-    
-      if cart[name] && cart[name][:count] >= coupon[:num] 
-        if cart["#{name} W/COUPON"] 
-          cart["#{name} W/COUPON"][:count] += 1 
-        else 
-          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
-          :clearance => cart[name][:clearance], :count => 1} 
-        end 
-  
-      cart[name][:count] -= coupon[:num] 
-    end 
-  end 
-end 
-  cart 
+  return cart if coupons == []
+
+  new_cart = cart
+
+  coupons.each do |coupon|
+    name = coupon[:item] 
+    num_of_c = coupon[:num]
+    if cart.include?(name) && cart[name][:count] >= num_of_c
+       #remove number of the new_cart's count
+       new_cart[name][:count] -= num_of_c
+       #increase the count when there is more items than the coupon allows
+       if new_cart["#{name} W/COUPON"]
+         new_cart["#{name} W/COUPON"][:count] += 1
+       #set the name with coupon with new value
+       else
+         new_cart["#{name} W/COUPON"] = {
+           :price => coupon[:cost],
+           :clearance => new_cart[name][:clearance],
+           :count => 1
+         }
+       end
+     end
+   end
+   new_cart
 end
 
 def apply_clearance(cart)

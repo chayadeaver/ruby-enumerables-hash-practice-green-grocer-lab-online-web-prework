0d0518d383c0f8e51b702791f1691d2cf639b09f
Chaya Deaver <chayadeaver@gmail.com>
2019-08-12 02:33:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,9 @@ def checkout(cart, coupons)
   clearance_cart = apply_clearance(coupon_cart) 
   clearance_cart.each do |item, attribute_hash| 
     total += (attribute_hash[:price] * attribute_hash[:count])
-  end 
+    new_cart.each do |name, hash|
+    total += (hash[:price] * hash[:count])
+  end
 total = (total * 0.9) if total > 100 
 
 end

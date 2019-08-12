e8fd3cb653f85779eed9602e68891ca5b690c518
Chaya Deaver <chayadeaver@gmail.com>
2019-08-12 01:45:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,7 +13,7 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coupons.each do |coupons|
+  coupons.each do |coupon|
     coupon.each do |attribute, value|
       name = coupon[:items]
       
